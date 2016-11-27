2cb1ff280994f689a7beb4f838632e72c8a276c1
Paul Gower <pgower@gmail.com>
2016-11-28 08:31:33 +1000

    Working on bootstrapper script

--- bootstraper/bootstrap.py
+++ bootstraper/bootstrap.py
@@ -2,13 +2,16 @@ import os
 import sys
 import argparse
 import shutil
-
+import logging
 # Expect that python2.7 and virtualenv are installed
 
+logger = logging.getLogger(__name__)
+
 class TemplateBootstrapper(object):
-    def __init__(self, name=None, target=None):
-        self.project_name = name
-        self.target_directory = os.path.abspath(target)
+    def __init__(self, project_name=None, target_directory=None, venv_target=None):
+        self.project_name = project_name
+        self.target_directory = os.path.abspath(target_directory)
+        self.venv_target = venv_target
 
         self.setup_virtualenv()
         self.setup_sphinx()
@@ -21,8 +24,9 @@ class TemplateBootstrapper(object):
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--name', type=str, help='Django project name', required=True)
-    parser.add_argument('--target', type=str, help='Target for the project', required=True)
+    parser.add_argument('project_name', help='Django project name')
+    parser.add_argument('--target_directory', '-t', type=str, help='Target for the project', default='./')
+    parser.add_argument('--venv_target', '-v', type=str, help='Virtualenv Target Directory', default='env')
 
     args = parser.parse_args(sys.argv[1:])
     kwargs = vars(args)
